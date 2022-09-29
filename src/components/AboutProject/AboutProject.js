041a13da6fec8432c0b571acd7ff4e40c7d68b5e
Konstantin Preis <kpreis@parallels.com>
2022-09-29 23:22:34 +0300

    TO-DO: need change AboutProject component from grid to flex

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -7,17 +7,21 @@ function AboutProject() {
     <div className="about-project">
       <div className="about-project__content">
         <h2 className="about-project__title">О проекте</h2>
-        <div className="about-project__grid-container">
-          <h3 className="about-project__part-title">Дипломный проект включал 5 этапов</h3>
-          <h3 className="about-project__part-title">На выполнение диплома ушло 5 недель</h3>
-          <p className="about-project__part-description">
-            Составление плана, работу над бэкендом, вёрстку,
-            добавление функциональности и финальные доработки.
-          </p>
-          <p className="about-project__part-description">
-            У каждого этапа был мягкий и жёсткий дедлайн,
-            которые нужно было соблюдать, чтобы успешно защититься.
-          </p>
+        <div className="about-project__flex-container">
+          <article className="about-project__article">
+            <h3 className="about-project__part-title">Дипломный проект включал 5&nbsp;этапов</h3>
+            <p className="about-project__part-description">
+              Составление плана, работу над бэкендом, вёрстку,
+              добавление функциональности и финальные доработки.
+            </p>
+          </article>
+          <article className="about-project__article">
+            <h3 className="about-project__part-title">На выполнение диплома ушло 5&nbsp;недель</h3>
+            <p className="about-project__part-description">
+              У каждого этапа был мягкий и жёсткий дедлайн,
+              которые нужно было соблюдать, чтобы успешно защититься.
+            </p>
+          </article>
         </div>
         <div className="about-project__timeline-grid-container">
           <div className="about-project__timeline about-project__timeline_first-week">1 неделя</div>
