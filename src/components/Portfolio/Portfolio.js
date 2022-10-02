1f0ce62cd9b895622f779c3fc95e4d9e2ad76c3f
Konstantin Preis <kpreis@parallels.com>
2022-10-02 12:45:51 +0300

    fix: changed background of the block Techs for mobile resolution, modified links in the block Portfolio and changed lang of project

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -9,16 +9,26 @@ function Portfolio() {
         <h2 className="portfolio__title">Портфолио</h2>
         <ul className="portfolio__list">
           <li className="portfolio__item">
-            <p className="portfolio__item-title">Статичный сайт</p>
-            <a className="portfolio__item-link" href="https://russian-travel.kpreis.ru/" target="_blank" rel="noopener noreferrer">↗</a>
+            <a className="portfolio__item-link" href="https://russian-travel.kpreis.ru/"
+              target="_blank" rel="noopener noreferrer">
+              <p className="portfolio__item-title">Статичный сайт</p>
+              <p className="portfolio__item-arrow">↗</p>
+            </a>
           </li>
           <li className="portfolio__item">
-            <p className="portfolio__item-title">Адаптивный сайт</p>
-            <a className="portfolio__item-link" href="https://mesto.kpreis.ru/" target="_blank" rel="noopener noreferrer">↗</a>
+            
+            <a className="portfolio__item-link" href="https://mesto.kpreis.ru/"
+              target="_blank" rel="noopener noreferrer">
+              <p className="portfolio__item-title">Адаптивный сайт</p>
+              <p className="portfolio__item-arrow">↗</p>
+            </a>
           </li>
           <li className="portfolio__item">
-            <p className="portfolio__item-title">Одностраничное приложение</p>
-            <a className="portfolio__item-link" href="https://mesto-react-auth.kpreis.ru/" target="_blank" rel="noopener noreferrer">↗</a>
+            <a className="portfolio__item-link" href="https://mesto-react-auth.kpreis.ru/"
+              target="_blank" rel="noopener noreferrer">
+              <p className="portfolio__item-title">Одностраничное приложение</p>
+              <p className="portfolio__item-arrow">↗</p>
+            </a>
           </li>
         </ul>
       </div>
